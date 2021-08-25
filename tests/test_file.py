ca5a691b8cc89b6f8e16dc40acf2ca87e3f2f53b
Luel Hagos <luelh0101@gmail.com>
2021-08-25 22:39:45 +0300

    breastCancer class added for the unit test

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,1 +1,30 @@
+import numpy as np
+import pandas as pd
+import unittest
+import sys
 
+import warnings
+warnings.filterwarnings("ignore")
+
+
+sys.path.append("../scripts/")
+
+from scripts.dataloader import data_loader
+
+df = data_loader('../data/data.csv')
+
+
+class breastCancer(unittest.TestCase):
+
+    def check_file_attributes(self):
+       col_len = len(list(df.columns))
+       assert col_len == 33, f"Function should return the value {33}, it is returning the value {col_len}"
+
+    def data_shape(self):
+        row_colmn = [df.shape[0], df.shape[1]]
+        assert row_colmn == [569, 33] , f"Function should return the value {[569, 33]}, it is returning the value {row_colmn}"
+
+
+
+if __name__ == '__main__':
+    unittest.main()
